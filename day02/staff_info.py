03df41aa8969670c21fa9a8ffa7470c85d3a040e
Dave <Linux@192.168.3.46>
2017-02-10 17:44:00 +0800

    update

--- day02/staff_info.py
+++ day02/staff_info.py
@@ -7,19 +7,26 @@ stuff = {
 '2':{'name':'Tom','sex':'male','age':20,'phone':'123456789'},
 '3':{'name':'Lily','sex':'female','age':30,'phone':'133336789'},
 '4':{'name':'Rony','sex':'male','age':40,'phone':'143456789'},
-'5':{'name':'Steven','sex':'male','age':50,'phone':'153456789'}
+'5':{'name':'Steven','sex':'male','age':50,'phone':'153456789'},
+'6':{'name':'Jack','sex':'female','age':55,'phone':'163456789'}
 }
 
 while True:
-	id = raw_input("Please input stuff id number:")
+	query = raw_input("Please input stuff id number:")
 
-	if id == "quit":
+	if query == "quit":
 		sys.exit('Goodbye')
 	else:
 		pass
 
-	if stuff.has_key(id):
-		print "id:%s name:%s sex:%s age:%d phone:%s" %(id,stuff[id]['name'],stuff[id]['sex'],stuff[id]['age'],stuff[id]['phone'])
-	else:
-		print "The stuff id is not exist"
-		continue
+	for k,v in stuff.items():
+		if query == k:
+			print k,v
+			break
+		else:
+			for i,j in v.items():
+				if query == i or query == j:
+					print k,v
+					break
+				else:
+					continue
