e14c1ec300820680f1a74a2bcea5a724453adfb6
Dave <Linux@192.168.3.46>
2017-02-20 15:56:24 +0800

    update

--- day03/credit_card.py
+++ day03/credit_card.py
@@ -4,6 +4,7 @@
 import sys,os
 import time
 import pickle
+import getpass
 
 exec_time = time.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -37,11 +38,12 @@ def check_user():
 		card_num = raw_input("请输入您的卡号:")
 		if bank_users.has_key(card_num):
 			if bank_users[card_num][5] == 1:
-				sys.exit("\033[31;1m卡已经被锁定,请联系管理员解锁\033[0m")	
+				sys.exit("\033[31;1m该卡已经被锁定,请联系管理员解锁\033[0m")	
 			while pass_try > 0:
-				card_passwd = raw_input("请输入信用卡密码,还剩%d次:" % pass_try)
+				#card_passwd = raw_input("请输入信用卡密码,还剩%d次:" % pass_try)
+				card_passwd = getpass.getpass("请输入信用卡密码,还剩%d次:" % pass_try).strip()
 				if card_passwd == bank_users[card_num][1]:
-					print "Welcome:\033[31;1m%s\033[0m,时间:%s" %(bank_users[card_num][0],exec_time)
+					#print "Welcome:\033[31;1m%s\033[0m,时间:%s" %(bank_users[card_num][0],exec_time)
 					break
 				else:
 					print "\033[31;1m密码不正确,请重新输入\033[0m"		
@@ -60,6 +62,8 @@ def check_user():
 
 #显示菜单
 def show_menu():
+	os.system("clear")
+	print "用户:\033[31;1m%s\033[0m,时间:%s" %(bank_users[card_num][0],exec_time)
 	print "\033[31;1m\t\t\t\t\t\t    *菜单*\033[0m",
         print '''
 		 +------------------------------------------------------------------------------+
@@ -82,11 +86,15 @@ def extract_money():
 			bank_users[card_num][3] -= money
 			bank_users[card_num][4] += (1+0.05)*money
 			print "\033[33;1m本次提取现金:%d¥,手续费为:%d¥\033[0m" %(money,money*0.05)
+			ret = raw_input("\033[31;1m是否继续提现(y继续,回车返回主菜单)\033[0m")
+			if ret == 'y':
+				continue
+			else:
+				break
 			checklist[card_num].append((exec_time,'提现',money,money*0.05))
 			f_write('checklist.txt',checklist)
 			f_write('bank_users.txt',bank_users)
 			break
-	pass
 
 #还款
 def repayment():
@@ -109,32 +117,42 @@ def repayment():
 				checklist[card_num].append((exec_time,'还款',float(repay),0.0))
 				f_write('checklist.txt',checklist)
 				f_write('bank_users.txt',bank_users)
-				break
+				ret = raw_input("y继续还款,q返回主菜单:")
+				if ret == 'y':
+					continue
+				else:
+					break
 	else:
 		print("\033[31;1m你的信用良好，没有欠款需要偿还!\033[0m")
-	pass
 
 #购买
 def shopping():
 	print "商品列表"
 	for k,v in products.items():
 		print "%s:%s¥" %(k,v)
-	buy = raw_input('请输入你要购买的商品:')
-	if bank_users[card_num][3] - products[buy] >= 0:
-		bank_users[card_num][3] -= products[buy]
-		bank_users[card_num][4] += products[buy]	
-		checklist[card_num].append((exec_time,'购买' + buy,products[buy],0.0))
-		f_write('checklist.txt',checklist)
-		f_write('bank_users.txt',bank_users)
-	else:
-		print "\033[31;1m你的信用卡余额不足"
-			
-	pass
+	while True:
+		buy = raw_input('请输入你要购买的商品(q返回):')
+		if bank_users[card_num][3] - products[buy] >= 0:
+			bank_users[card_num][3] -= products[buy]
+			bank_users[card_num][4] += products[buy]	
+			checklist[card_num].append((exec_time,'购买' + buy,products[buy],0.0))
+			f_write('checklist.txt',checklist)
+			f_write('bank_users.txt',bank_users)
+			ret = raw_input("y继续购买,q返回主菜单:")
+			if ret == 'y':
+				continue
+			else:
+				break
+		elif buy == 'q':
+			break
+		else:
+			print "\033[31;1m你余额不足以购买,请选择购买其他商品"
+			continue
 
 #转账功能
 def bank_transfer():
 	while True:
-		rec_card = raw_input("请输入你要转入的卡号:").strip()
+		rec_card = raw_input("请输入你要转入的卡号(q返回):").strip()
 		if bank_users.has_key(rec_card):
 			tran_money = int(raw_input("请输入你要转入的金额:"))
 			confirm = raw_input("确认你要输入的卡号为:%s,用户名为:%s,转入的金额为:%d(y/n/q)" %(rec_card,bank_users[rec_card][0],tran_money))
@@ -146,27 +164,37 @@ def bank_transfer():
 				f_write('bank_users.txt',bank_users)
 				f_write('checklist.txt',checklist)
 				print "\033[31;1m转账成功\033[0m"
-				break
+				ret = raw_input("y继续转账,回车返回主菜单:")
+				if ret == 'y':
+					continue
+				else:
+					break
 			elif confirm == 'n':
 				continue
 			else:
 				break
-		
+		elif rec_card == 'q':
+			break
 		else:
 			print "你输入的账号不存在!"
 			continue
 
 #账单查询
 def bill_check():
-	checklist = f_read('checklist.txt')
-	print_format = '|  %15s  |    %-13s |  %-7s  |  %-6s  |'
-	print '''\t\t\t   \033[31;1m信用卡账单\033[0m
+	while True:
+		checklist = f_read('checklist.txt')
+		print_format = '|  %15s  |    %-13s |  %-7s  |  %-6s  |'
+		print '''\t\t\t   \033[31;1m信用卡账单\033[0m
 +-----------------------+----------------+-----------+----------+
 |  %14s         |  %10s      |   %-8s  |  %-6s  |
 +-----------------------+----------------+-----------+----------+''' % ('时间','操作','金额','手续费')
-	for i in checklist[card_num]:
-		print print_format % i
-	pass
+		for i in checklist[card_num]:
+			print print_format % i
+		ret = raw_input("回车返回主菜单,q退卡:")	
+		if ret == 'q':
+			sys.exit("Goodbye")
+		else:
+			break
 
 #定义一个读函数,参数(文件名)
 def f_read(filename):
@@ -193,6 +221,9 @@ if __name__ == '__main__':
 		bank_users = f_read('bank_users.txt')
 	else:
 		f_write('bank_users.txt',bank_users)
+
+	#卡解锁
+	#bank_users['95588'][5] = 0
 
 	check_user()
 
